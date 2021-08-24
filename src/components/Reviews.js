5d93a6ad7028bf8e5ffefbab82c7bf48e3c5c3b3
Chelsea <chelsea.icaza10@hotmail.com>
2021-08-24 00:58:17 -0400

    Log-In Form

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
 function Reviews(props) {
 
+    const [username, setUsername] = useState(" ")
+    console.log("useState Username is: ", username)
 
-    // const handleSubmit = () => {
-    //     props.addReview
-    // }
+
+     const handleSubmit = (e) => {
+         e.preventDefault()
+        //props.addReview
+     }
 
 
 
 
     return(
         <>
-        <form 
-        // onSubmit={handleSubmit}
-        >
+        <form onSubmit={handleSubmit} >
+            {/* onSubmit={handleSubmit} */}
+            <h1>Log-in </h1>
+            <label htmlFor="username"> Username </label>
+            <input type="text" 
+            id="username" 
+            value={username}
+            onChange ={ e => {
+            setUsername(e.target.value) 
+            //console.log("Username after Submit, " , username)
+            }}/>
+
+            <label htmlFor="password"> Password </label>
+            <input type="text" id="password" />
+
+            <input type="submit" value="Enter" />
 
         </form>
         </>
